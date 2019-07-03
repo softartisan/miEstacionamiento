0e2cc2ae52c2970e06dfd99aadd3ade8a6d4d878
softartisan <slcaniob@gmail.com>
2019-07-03 02:34:11 -0400

    agrego mapa busqueda

--- resources/views/arrendar/arrendar.blade.php
+++ resources/views/arrendar/arrendar.blade.php
@@ -2,72 +2,17 @@
 
 
 @section('imports')
-<title>Map</title>
-	<style>
-#myMap {
-   height: 350px;
-   width: 680px;
-}
-
+<style>
+ #map-canvas {
+      height: 400px;
+    }
+    /* Optional: Makes the sample page fill the window. */
+    html, body {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+    }
 </style>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwkqnh-BCwN7SFu-d2BBL8p7otc66uzlE&amp;sensor=false"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
-</script>
-<script type="text/javascript"> 
-var map;
-var marker;
-var myLatlng = new google.maps.LatLng(-33.433332,-70.615599);
-var geocoder = new google.maps.Geocoder();
-var infowindow = new google.maps.InfoWindow();
-function initialize(){
-var mapOptions = {
-zoom: 18,
-center: myLatlng,
-mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
-map = new google.maps.Map(document.getElementById("myMap"), mapOptions);
-
-marker = new google.maps.Marker({
-map: map,
-position: myLatlng,
-draggable: true 
-}); 
-
-geocoder.geocode({'latLng': myLatlng }, function(results, status) {
-if (status == google.maps.GeocoderStatus.OK) {
-if (results[0]) {
-$('#latitude,#longitude').show();
-$('#address').val(results[0].formatted_address);
-$('#latitude').val(marker.getPosition().lat());
-$('#longitude').val(marker.getPosition().lng());
-infowindow.setContent(results[0].formatted_address);
-infowindow.open(map, marker);
-}
-}
-});
-
-google.maps.event.addListener(marker, 'dragend', function() {
-
-geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
-if (status == google.maps.GeocoderStatus.OK) {
-if (results[0]) {
-$('#address').val(results[0].formatted_address);
-$('#latitude').val(marker.getPosition().lat());
-$('#longitude').val(marker.getPosition().lng());
-infowindow.setContent(results[0].formatted_address);
-infowindow.open(map, marker);
-}
-}
-});
-});
-
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-
-
-
 @endsection
 
 @section('content')
@@ -75,24 +20,29 @@ google.maps.event.addDomListener(window, 'load', initialize);
 <div class="container">
     <div class="row">
       <div class="col-md-8">
-          <div id="myMap"></div>
+          <div id="map-canvas"></div>
       </div>
       <div class="col-md-4 mt-5 mt-md-0">
           <div class="card bg-dark text-white ">
               <h5 class="card-header">Selecciona tu ubicación en el mapa</h5>
               <div class="card-body">
-                <p>Selecciona el lugar donde estas para buscar estacionamiento cercano</p>
+                <p>Selecciona o busca tu dirección para buscar estacionamientos cercanos </p>
                 {{-- <button onclick="getLocation()" class="btn btn-outline-light btn-block">Utilizar mi dirección</button> --}}
                 <form method="POST" action="/arrendar/busqueda">
                   @csrf
-                    <input id="address" class="form-control" type="hidden" name="direccion"/><br/>
-                    <input type="hidden" id="latitude" name="latitud" placeholder="Latitude"/>
-                    <input type="hidden" id="longitude" name="longitud" placeholder="Longitude"/>
-                  <button class="btn btn-block btn-light">Buscar</button>
+                  <input id="map-search" name="direccion" class="form-control" type="text" placeholder="Tu dirección">
+                  <input type="hidden" name="lat" class="latitude"></label>
+                  <input type="hidden" name="lon" class="longitude"></label>
+                  <input type="hidden" class="reg-input-city" placeholder="City"></label>
+                  
+              
+                  <button class="btn btn-block btn-light mt-3">Buscar</button>
                 </form>
               </div>
       </div>
     </div>
 </div>
 
+<script src="/js/mapa.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAl8VWHE7KL4wcDz6tzOMTg17ZtAhsu8tA&libraries=places&callback=initialize"></script>
 @endsection
